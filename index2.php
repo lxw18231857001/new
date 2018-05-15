c229647486dc0c06ddedad48da1c90d227080494
lxw <1844912514@qq.com>
2018-05-15 15:27:26 +0800

    添加index2.php

---
+++ index2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/15
+ * Time: 14:52
+ */
+class index{
+    public function index()
+    {
+        echo 1111;
+        echo 222;
+        echo 3333;
+    }
+}
