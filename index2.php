1fd797751c11939f9d8a5a705f3428ce383d2bc9
lxw <1844912514@qq.com>
2018-05-15 16:18:37 +0800

    index2

--- index2.php
+++ index2.php
@@ -11,5 +11,13 @@ class index{
         echo 1111;
         echo 222;
         echo 3333;
+        echo 4444;
+    }
+    public function index2()
+    {
+        echo 1111;
+        echo 222;
+        echo 3333;
+        echo 4444;
     }
 }
