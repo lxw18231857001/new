d551819c69e28f0b63aef77f4e6765ad3d97f887
lxw <1844912514@qq.com>
2018-05-15 16:48:59 +0800

    'all'

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/14
+ * Time: 12:54
+ */
+
+class index
+{
+    public function index()
+    {
+        
+    }
+
+    public function demo()
+    {
+        $arr=array_pop();
+        var_dump($arr);
+        $arr=array_pop();
+        var_dump($arr);
+    }
+}
